d1316bfbeafdf2ca37d7f92e364a347bb1397a47
gw2jhonny <jhonny.matute@unisimon.edu.co>
2023-05-23 22:34:35 -0500

    Mejoras
    Composer update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FormValidtionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "API";
 });
+
+Route::get('/menu', function () {
+    return view('menu');
+});
+
+
+
+
+
+Route::get('/form', [FormValidtionController::class, 'createUserForm']);
+Route::post('/form', [FormValidtionController::class, 'UserForm'])->name('validate.form');
+
